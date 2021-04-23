7df1d5abfc88d44be202e3041969ac25ca8c0e26
Dave Nottage <davidn@radsoft.com.au>
2021-04-23 12:46:38 +0930

    Add support for HTTP v1 API

--- Core/PI.Consts.pas
+++ Core/PI.Consts.pas
@@ -3,7 +3,10 @@ unit PI.Consts;
 interface
 
 const
-  cFCMSendURL = 'https://fcm.googleapis.com/fcm/send';
+  cFCMLegacyHTTPSendURL = 'https://fcm.googleapis.com/fcm/send';
+  cFCMHTTPv1SendURL = 'https://fcm.googleapis.com/v1/projects/%s/messages:send'; // eg https://fcm.googleapis.com/v1/projects/delphi-worlds-test/messages:send
+  cFCMJWTScopes = 'https://www.googleapis.com/auth/firebase.messaging';  // https://developers.google.com/identity/protocols/oauth2/scopes
+  cFCMJWTAudience = 'https://oauth2.googleapis.com/token'; // 'https://www.googleapis.com/oauth2/v4/token';
 
 implementation
 
